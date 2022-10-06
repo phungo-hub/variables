806ecc263b05016645e9082966186709aaec8cb5
phungo-hub <ngokieuphu@gmail.com>
2022-10-06 10:28:48 +0700

    add files

---
+++ variables/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5d;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+
+        System.out.println("i is: " + i);
+        System.out.println("f is: " + f);
+        System.out.println("d is: " + d);
+        System.out.println("b is: " + b);
+        System.out.println("c is: " + c);
+        System.out.println("s is: " + s);
+    }
+}
